533227dfc34fa0b48040fd03b77fa542405479f1
David Bacsa <bacsadave@gmail.com>
2023-05-17 19:52:08 +0200

    Added Horizontal Support

--- configuration.py
+++ configuration.py
@@ -3,7 +3,9 @@ import ast
 
 config = configparser.ConfigParser()
 inventory_coords = None
+h_inventory_coords = None
 stash_coords = None
+h_stash_coords = None
 
 def create_stash_coords():
     stash_width = int(config["STASH"][f"end_x{inv_suffix}"]) - int(config["STASH"][f"start_x{inv_suffix}"])
@@ -17,6 +19,18 @@ def create_stash_coords():
 
     return coords
 
+def create_h_stash_coords():
+    stash_width = int(config["STASH"][f"end_x{inv_suffix}"]) - int(config["STASH"][f"start_x{inv_suffix}"])
+    stash_height = int(config["STASH"][f"end_y{inv_suffix}"]) - int(config["STASH"][f"start_y{inv_suffix}"])
+    step_x = stash_width / 48
+    step_y = stash_height / 48
+    coords = []
+    for i in range(1, 49, 2):
+        for j in range(1, 49, 2):
+            coords.append((round(int(config["STASH"][f"start_x{inv_suffix}"]) + step_x * j), round(int(config["STASH"][f"start_y{inv_suffix}"]) + step_y *  i)))
+
+    return coords
+
 
 def create_inventory_coords():
     inv_width = int(config["INVENTORY"][f"end_x{inv_suffix}"]) - int(config["INVENTORY"][f"start_x{inv_suffix}"])
@@ -27,6 +41,21 @@ def create_inventory_coords():
     for i in range(1, 25, 2):
         for j in range(1, 11, 2):
             coords.append((round(int(config["INVENTORY"][f"start_x{inv_suffix}"]) + step_x * i), round(int(config["INVENTORY"][f"start_y{inv_suffix}"]) + step_y *  j)))
+
+    return coords
+
+def create_h_inventory_coords():
+    inv_width = int(config["INVENTORY"][f"end_x{inv_suffix}"]) - int(config["INVENTORY"][f"start_x{inv_suffix}"])
+    inv_height = int(config["INVENTORY"][f"end_y{inv_suffix}"]) - int(config["INVENTORY"][f"start_y{inv_suffix}"])
+    step_x = inv_width / 24
+    step_y = inv_height / 10
+    coords = []
+    # for i in range(1, 25, 2):
+    #     for j in range(1, 11, 2):
+    #         coords.append((round(int(config["INVENTORY"][f"start_x{inv_suffix}"]) + step_x * i), round(int(config["INVENTORY"][f"start_y{inv_suffix}"]) + step_y *  j)))
+    for i in range(1, 11, 2):
+        for j in range(1, 25, 2):
+            coords.append((round(int(config["INVENTORY"][f"start_x{inv_suffix}"]) + step_x * j), round(int(config["INVENTORY"][f"start_y{inv_suffix}"]) + step_y *  i)))
 
     return coords
 
@@ -53,4 +82,6 @@ inv_suffix = ""
 if config["SETTINGS"]["widescreen"].lower() == "true":
     inv_suffix = "_1080p_wide"
 inventory_coords = create_inventory_coords()
+h_inventory_coords = create_h_inventory_coords()
 stash_coords = create_stash_coords()
+h_stash_coords = create_h_stash_coords()
